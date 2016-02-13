46a06292264a7eddc4b9df254b8337d7a00e783e
PBrockmann <Patrick.Brockmann@lsce.ipsl.fr>
2016-02-13 14:35:29 +0100

    - Update dc.js, d3.js - New dataset (plus one for testing proxies_select.tsv) - Replace Proxy by Material and Archive - Clear bins settings for bar charts - Add page loader - Changes on contact page

--- setup.js
+++ setup.js
@@ -1,4 +1,5 @@
 
+//====================================================================
 var map;
 var markers = [] ;
 var markerGroup ;
@@ -8,37 +9,38 @@ var grat;
 var filter;
 var depthDimension;
 var depthGrouping;
-var oldestDateDimension;
-var oldestDateGrouping;
-var proxyDimension;
-var proxyGrouping;
-var charts;
-var domCharts;
+var ageDimension;
+var ageGrouping;
+var archiveDimension;
+var archiveGrouping;
+var materialDimension;
+var materialGrouping;
 
 var latDimension;
-var lonDimension;
+var lngDimension;
 var idDimension;
 var idGrouping;
 
+//====================================================================
 function init() {
 
-d3.tsv("proxies.tsv", function(data) {
-    data.forEach(function(d) {
-	d.lng= +d.Longitude;
-	d.lat= +d.Latitude;
-	d.depth= +d.Depth;
-	d.oldestDate= +d.OldestDate;
-	d.recentDate= +d.RecentDate;
-	d.proxy= +d.Proxy;
-	});
-     points=data;
+d3.tsv("proxies_select.tsv", function(data) {
+//d3.tsv("proxies.tsv", function(data) {
+  data.forEach(function(d) {
+	d.Longitude = +d.Longitude;
+	d.Latitude = +d.Latitude;
+	d.Depth = +d.Depth;
+	d.OldestDate = +d.OldestDate;
+	d.RecentDate = +d.RecentDate;
+  });
+  points=data;
 
   initMap();
   initCrossfilter();
 
-// bind map bounds to lat/lon filter dimensions
-  latDimension = filter.dimension(function(p) { return p.lat; });
-  lonDimension = filter.dimension(function(p) { return p.lng; });
+// bind map bounds to lat/lng filter dimensions
+  latDimension = filter.dimension(function(d) { return d.Latitude; });
+  lngDimension = filter.dimension(function(d) { return d.Longitude; });
 
   map.on("moveend", function() {
     var bounds = map.getBounds();
@@ -47,13 +49,13 @@ d3.tsv("proxies.tsv", function(data) {
 
     // NOTE: need to be careful with the dateline here
     latDimension.filterRange([southWest.lat, northEast.lat]);
-    lonDimension.filterRange([southWest.lng, northEast.lng]);
+    lngDimension.filterRange([southWest.lng, northEast.lng]);
 
     update1();
   });
 
 // dimension and group for looking up currently selected markers
-  idDimension = filter.dimension(function(p, i) { return i; });
+  idDimension = filter.dimension(function(d, i) { return i; });
   idGrouping = idDimension.group(function(id) { return id; });
 
   // Render the total.
@@ -69,6 +71,7 @@ d3.tsv("proxies.tsv", function(data) {
 }
 
 
+//====================================================================
 function initMap() {
 
 var mapmadeUrl = 'http://services.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}',
@@ -110,15 +113,15 @@ customMarker = L.Marker.extend({
 
 // create array of markers from points and add them to the map
 for (var i = 0; i < points.length; i++) {
-   //markers[i] = new L.Marker([point.lat, point.lng], {icon: myIcon});
-   markers[i] = new customMarker([points[i].lat, points[i].lng], {icon: myIcon, Id: (i+1).toString()});
+   //markers[i] = new L.Marker([point.Latitude, point.Longitude], {icon: myIcon});
+   markers[i] = new customMarker([points[i].Latitude, points[i].Longitude], {icon: myIcon, Id: (i+1).toString()});
    markers[i].bindPopup(
-		  "Id: #" + "<b>" + (i+1).toString() + "</b> "
-		+ "Core: " + "<b>" + points[i].Core + "</b></br>"
-		+ "Position: " + "<b>" + points[i].lng.toFixed(2) + "°E</b>, <b>" + points[i].lat.toFixed(2) + "°N</b></br>"
-		+ "Depth (m): " + "<span style='color: #2EA3DB;'><b>" +  points[i].depth.toFixed(2) + "</b></span></br>"
-		+ "Date (ka): " + "<span style='color: #C9840B;'>" + "from <b>" + points[i].recentDate.toFixed(2) + "</b> to <b>" + points[i].oldestDate.toFixed(2) + "</b></span></br>"
-		+ "Proxy: " + "<b>" + points[i].Proxy + "</b></br>"
+		  "Id: " + "<b>" + points[i].Id + "</b></br>"
+		+ "Position: " + "<b>" + points[i].Longitude.toFixed(2) + "°E</b>, <b>" + points[i].Latitude.toFixed(2) + "°N</b></br>"
+		+ "Depth (m): " + "<span style='color: #2EA3DB;'><b>" +  points[i].Depth.toFixed(2) + "</b></span></br>"
+		+ "Date (ka): " + "<span style='color: #C9840B;'>" + "from <b>" + points[i].RecentDate.toFixed(2) + "</b> to <b>" + points[i].OldestDate.toFixed(2) + "</b></span></br>"
+		+ "Archive: " + "<b>" + points[i].Archive + "</b></br>"
+		+ "Material: " + "<b>" + points[i].Material + "</b></br>"
 		,{autoPan: true, keepInView: true, closeOnClick: false}
 		);
    markers[i].on('mouseover', function(e) {
@@ -141,85 +144,128 @@ map.addLayer(markerGroup);
 
 }
 
+//====================================================================
 function initCrossfilter() {
+
+  //-----------------------------------
   filter = crossfilter(points);
 
-  // simple dimensions and groupings for major variables
-  depthDimension = filter.dimension(
-      function(p) {
-        return p.depth;
+  //-----------------------------------
+  depthRange = [0., 5000.];
+  depthBinWidth = 100.;
+  depthDimension = filter.dimension( function(d) { 
+	// Threshold
+	var depthThresholded = d.Depth;
+	if (depthThresholded <= depthRange[0]) depthThresholded = depthRange[0];
+	if (depthThresholded >= depthRange[1]) depthThresholded = depthRange[1] - depthBinWidth;
+	return depthBinWidth*Math.floor(depthThresholded/depthBinWidth);
       });
-  depthGrouping = depthDimension.group(
-      function(v) {
-        return Math.floor(v);
-      });
+  depthGrouping = depthDimension.group();
 
-  oldestDateDimension = filter.dimension(
-      function(p) {
-	// Threshold age
-	if (p.oldestDate >= 500) p.oldestDate = 499;
-	if (p.oldestDate <= 0) p.oldestDate = -1;
-        return p.oldestDate;
+  ageRange = [-10., 500.];
+  ageBinWidth = 10.;
+  ageDimension = filter.dimension( function(d) {
+	// Threshold
+	var ageThresholded = d.OldestDate;
+	if (ageThresholded <= ageRange[0]) ageThresholded = ageRange[0];
+	if (ageThresholded >= ageRange[1]) ageThresholded = ageRange[1]- ageBinWidth;
+        return ageBinWidth*Math.floor(ageThresholded/ageBinWidth);
       });
-  oldestDateGrouping = oldestDateDimension.group(
-      function(v) {
-        return Math.floor(v);
-      });
+  ageGrouping = ageDimension.group();
 
-  proxyDimension = filter.dimension(
-      function(p) {
-        return p.Proxy;
-      });
-  proxyGrouping = proxyDimension.group();
+  archiveDimension = filter.dimension( function(d) { return d.Archive; });
+  archiveGrouping = archiveDimension.group();
 
-  depthChart  = dc.barChart("#chart-depth"),
-  ageChart  = dc.barChart("#chart-age"),
-  proxyChart  = dc.rowChart("#chart-proxy"),
+  materialDimension = filter.dimension( function(d) { return d.Material; });
+  materialGrouping = materialDimension.group();
 
+  //-----------------------------------
+  depthChart  = dc.barChart("#chart-depth");
+  ageChart  = dc.barChart("#chart-age");
+  archiveChart  = dc.rowChart("#chart-archive");
+  materialChart  = dc.rowChart("#chart-material");
+
+  //-----------------------------------
   depthChart
-    .width(400)
+    .width(380)
     .height(200)
+    .margins({top: 10, right: 20, bottom: 30, left: 30})	
     .centerBar(false)
     .elasticY(true)
-    .xUnits(function(){return 50;})
     .dimension(depthDimension)
     .group(depthGrouping)
     .on("preRedraw",update0)
-    .x(d3.scale.linear()
-          .domain([0, 5000]))
+    .x(d3.scale.linear().domain(depthRange))
+    .xUnits(dc.units.fp.precision(depthBinWidth))
+    .round(function(d) {return depthBinWidth*Math.floor(d/depthBinWidth)})
+    .gap(0)
     .renderHorizontalGridLines(true);
 
   xAxis_depthChart = depthChart.xAxis();
-  xAxis_depthChart.ticks(6).tickFormat(d3.format(".0f"));
+  xAxis_depthChart.ticks(6).tickFormat(d3.format("d"));
+  yAxis_depthChart = depthChart.yAxis();
+  yAxis_depthChart.tickFormat(d3.format("d")).tickSubdivide(0);
 
+  //-----------------------------------
   ageChart
-    .width(400)
+    .width(380)
     .height(200)
+    .margins({top: 10, right: 20, bottom: 30, left: 30})	
     .colors(["#F5B441"])
     .centerBar(false)
     .elasticY(true)
-    .xUnits(function(){return 50;})
-    .dimension(oldestDateDimension)
-    .group(oldestDateGrouping)
-    .x(d3.scale.linear()
-          .domain([-10, 500]))
+    .dimension(ageDimension)
+    .group(ageGrouping)
+    .on("preRedraw",update0)
+    .x(d3.scale.linear().domain(ageRange))
+    .xUnits(dc.units.fp.precision(ageBinWidth))
+    .round(function(d) {return ageBinWidth*Math.floor(d/ageBinWidth)})
+    .gap(0)
     .renderHorizontalGridLines(true);
 
-  proxyChart
-    .width(200)
+  xAxis_ageChart = ageChart.xAxis();
+  xAxis_ageChart.ticks(6).tickFormat(d3.format("d"));
+  yAxis_ageChart = ageChart.yAxis();
+  yAxis_ageChart.tickFormat(d3.format("d")).tickSubdivide(0);	//tickSubdivide(0) still 1 subtick
+
+  //-----------------------------------
+  Ice_color = "#d6dbe0";
+  Lake_color = "#6d87a8";
+  Ocean_color = "#008cb2";
+  Speleothem_color = "#afa393";
+  Tree_color = "#568e14";
+  var archiveColors = d3.scale.ordinal()
+   	.range([Ice_color, Lake_color, Ocean_color, Speleothem_color, Tree_color]);
+
+  archiveChart
+    .width(180)
     .height(200)
-    .margins({top: 10, right: 10, bottom: 30, left: 10})		// Default margins: {top: 10, right: 50, bottom: 30, left: 30}.
-    .dimension(proxyDimension)
-    .group(proxyGrouping)
-    .colors(d3.scale.category20()) 
+    .margins({top: 10, right: 10, bottom: 30, left: 10})	
+    .dimension(archiveDimension)
+    .group(archiveGrouping)
+    .colors(archiveColors)
     .elasticX(true)
-    .gap(0)
+    .gap(2)
     .xAxis().ticks(4);
 
+  //-----------------------------------
+  materialChart
+    .width(180)
+    .height(200)
+    .margins({top: 10, right: 10, bottom: 30, left: 10})	
+    .dimension(materialDimension)
+    .group(materialGrouping)
+    .colors(d3.scale.category10()) 
+    .elasticX(true)
+    .gap(2)
+    .xAxis().ticks(4);
+
+  //-----------------------------------
   dc.renderAll();
 
 }
 
+//====================================================================
 // set visibility of markers based on crossfilter
 function updateMarkers() {
   var pointIds = idGrouping.all();
@@ -231,6 +277,7 @@ function updateMarkers() {
   }
 }
 
+//====================================================================
 // Update map markers, list and number of selected
 function update0() {
   updateMarkers();
@@ -238,6 +285,7 @@ function update0() {
   d3.select("#active").text(filter.groupAll().value());
 }
 
+//====================================================================
 // Update dc charts, map markers, list and number of selected
 function update1() {
   dc.redrawAll();
@@ -260,89 +308,115 @@ function update1() {
   }
 }
 
+//====================================================================
 function initList() {
   var proxyItem = d3.select("#proxiesListTitle")
   		.append("div")
-   		.style("background", "#ddd")
-   		.style("font-style", "italic")
   		.attr("class", "row");
   proxyItem.append("div")
    	.attr("class", "col-md-1")
+   	.style("width", "50px")
    	.text("Id");
   proxyItem.append("div")
-   	.attr("class", "col-md-2")
-   	.text("Core");
-  proxyItem.append("div")
    	.attr("class", "col-md-1")
+   	.style("width", "80px")
    	.style("text-align", "right")
    	.text("Depth");
   proxyItem.append("div")
    	.attr("class", "col-md-1")
    	.style("text-align", "right")
+   	.text("Most recent");
+  proxyItem.append("div")
+   	.attr("class", "col-md-1")
+   	.style("text-align", "right")
    	.text("Oldest");
   proxyItem.append("div")
-   	.attr("class", "col-md-2")
-   	.style("text-align", "right")
-   	.text("Proxy");
+   	.attr("class", "col-md-1")
+   	.style("text-align", "left")
+   	.text("Archive");
   proxyItem.append("div")
    	.attr("class", "col-md-2")
-   	.style("text-align", "right")
-   	.text("Species");
+   	.style("text-align", "left")
+   	.text("Material");
+  proxyItem.append("div")
+        .attr("class", "col-md-2")
+   	.style("text-align", "left")
+   	.text("DOI");
   proxyItem.append("div")
         .attr("class", "col-md-3")
-   	.style("text-align", "right")
+   	.style("width", "350px")
+   	.style("text-align", "left")
    	.text("Reference");
+
+  format1 = d3.format(".0f");
+  format2 = d3.format(".2f");
 
   var pointIds = idGrouping.all();
   for (var i = 0; i < pointIds.length; i++) {
   	var proxyItem = d3.select("#proxiesList")
     			.append("div")
     			.attr("class", "proxyItem row")
-         		.attr("id", (i+1).toString())
-			.on('click', popupfromlist);
+         		.attr("id", (i+1).toString());
   	proxyItem.append("div")
          	.attr("class", "col-md-1")
-         	.attr("title", "#"+(i+1).toString())
-         	.text("#"+(i+1).toString());
-  	proxyItem.append("div")
-         	.attr("class", "col-md-2")
-         	.attr("title", points[i].Core)
-         	.text(points[i].Core);
+   		.style("width", "50px")
+         	.attr("title", "#"+ points[i].Id)
+         	.text("#"+points[i].Id)
+		.on("mouseover", function() { d3.select(this).style("font-weight", "bold"); })
+		.on("mouseout", function() { d3.select(this).style("font-weight", "normal"); })
+		.on('click', popupfromlist);
   	proxyItem.append("div")
          	.attr("class", "col-md-1")
+   		.style("width", "80px")
          	.style("text-align", "right")
 		.style("color", "#2EA3DB")
          	.attr("title", points[i].Depth)
-         	.text(points[i].Depth);
+         	.text(format1(points[i].Depth));
   	proxyItem.append("div")
          	.attr("class", "col-md-1")
          	.style("text-align", "right")
 		.style("color", "#F5B441")
+                .attr("title", points[i].RecentDate)
+                .text(format2(points[i].RecentDate));
+        proxyItem.append("div")
+                .attr("class", "col-md-1")
+                .style("text-align", "right")
+                .style("color", "#F5B441")
          	.attr("title", points[i].OldestDate)
-         	.text(points[i].OldestDate);
+         	.text(format2(points[i].OldestDate));
+  	proxyItem.append("div")
+         	.attr("class", "col-md-1")
+         	.style("text-align", "left")
+         	.attr("title", points[i].Archive)
+         	.text(points[i].Archive);
   	proxyItem.append("div")
          	.attr("class", "col-md-2")
-         	.style("text-align", "right")
-         	.attr("title", points[i].Proxy)
-         	.text(points[i].Proxy);
+         	.style("text-align", "left")
+         	.attr("title", points[i].Material)
+         	.text(points[i].Material);
   	proxyItem.append("div")
          	.attr("class", "col-md-2")
-         	.style("text-align", "right")
-         	.attr("title", points[i].Species)
-         	.text(points[i].Species);
+         	.style("text-align", "left")
+         	.attr("title", points[i].DOI)
+         	.text(points[i].DOI)
+		.on("mouseover", function() { d3.select(this).style("color", "#0645AD"); })
+		.on("mouseout", function() { d3.select(this).style("color", "#333"); })
+		.on("click", function(d,i) { window.open("https://scholar.google.fr/scholar?q=" + points[i].DOI); });
   	proxyItem.append("div")
          	.attr("class", "col-md-3")
-         	.style("text-align", "right")
+   		.style("width", "350px")
+         	.style("text-align", "left")
          	.attr("title", points[i].Reference)
          	.text(points[i].Reference);
   }
 }
 
+//====================================================================
 function popupfromlist() {
-	var i = this.id - 1;
-	var lng = points[i].lng;
-	var lat = points[i].lat;
-	console.log(i, lng.toFixed(2), lat.toFixed(2));
+	var id = d3.select(this).text().split('#').pop();
+	var i = id -1;
+	var lng = points[i].Longitude;
+	var lat = points[i].Latitude;
 	//map.setView(new L.LatLng(lat,lng), 6);
 	//map.panTo(new L.LatLng(lat,lng));
 	//markers[i].openPopup();
@@ -353,12 +427,13 @@ function popupfromlist() {
 				m.openPopup();
 			});
 	var container = $("#proxiesList");
-	var scrollTo = $("#" + this.id);
+	var scrollTo = $("#" + id);
 	container.scrollTop( scrollTo.offset().top - container.offset().top + container.scrollTop() );
         $(".proxyItem").css("font-weight", "normal");
 	$("#"+this.id).css("font-weight", "bold");
 }
 
+//====================================================================
 function updateList() {
   var pointIds = idGrouping.all();
   for (var i = 0; i < pointIds.length; i++) {
@@ -369,3 +444,4 @@ function updateList() {
   }
 }
 
+//====================================================================
---
+++ setup1.js
@@ -0,0 +1,74 @@
+
+//================================================================
+var filter;
+var depthDimension;
+var depthGrouping;
+
+//================================================================
+function init() {
+
+d3.tsv("depth.tsv", function(data) {
+  data.forEach(function(d) {
+	d.Depth = +d.Depth;
+  });
+  points=data;
+
+  initCrossfilter();
+});
+
+}
+
+//================================================================
+function initCrossfilter() {
+
+  //-----------------------------------
+  filter = crossfilter(points);
+
+  //-----------------------------------
+  depthRange = [0., 5000.];
+  depthBinWidth = 500.;
+  depthDimension = filter.dimension( function(d) { 
+	// Threshold
+	var depthThresholded = d.Depth;
+	if (depthThresholded <= depthRange[0]) depthThresholded = depthRange[0];
+	if (depthThresholded >= depthRange[1]) depthThresholded = depthRange[1] - depthBinWidth;
+	return depthBinWidth*Math.floor(depthThresholded/depthBinWidth);
+      });
+  depthGrouping = depthDimension.group();      	// by default reduceCount
+
+  //-----------------------------------
+  depthChart  = dc.barChart("#chart-depth");
+  dataTable  = dc.dataTable("#dataTable");
+
+  //-----------------------------------
+  depthChart
+    .width(380)
+    .height(200)
+    .margins({top: 10, right: 20, bottom: 30, left: 30})	
+    .centerBar(false)
+    .elasticY(true)
+    .dimension(depthDimension)
+    .group(depthGrouping)
+    .x(d3.scale.linear().domain(depthRange))
+    .xUnits(dc.units.fp.precision(depthBinWidth))
+    .round(function(d) {return depthBinWidth*Math.floor(d/depthBinWidth)})
+    .renderHorizontalGridLines(true);
+
+  xAxis_depthChart = depthChart.xAxis();
+  xAxis_depthChart.tickFormat(d3.format("d"));
+  yAxis_depthChart = depthChart.yAxis();
+  yAxis_depthChart.ticks(6).tickFormat(d3.format("d")).tickSubdivide(0);		// tickSubdivide(0) should remove sub ticks but not
+
+  //-----------------------------------
+  dataTable
+    .dimension(depthDimension)
+    .group(function (d) {
+            return d.Id + "   " + d.Depth;                        // Data table does not use crossfilter group but rather a closure as a grouping function
+        })
+    .size(30);
+
+  //-----------------------------------
+  dc.renderAll();
+
+}
+
